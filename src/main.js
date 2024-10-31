cd0f256e8998075212cbea47ee47c305c8c6f965
Zandiaffandi236 <marzandi.leta@gmail.com>
2024-10-31 18:32:27 +0700

    add users section

--- src/main.js
+++ src/main.js
@@ -18,6 +18,14 @@ ScrollReveal().reveal('.hero-reveal', {
   interval: 100  //
 })
 
+ScrollReveal().reveal('#partners', {
+  distance: '50px',
+  duration: 800,
+  easing: 'ease-out',
+  origin: 'bottom',
+  interval: 100  //
+})
+
 
 
 const hamMenu = document.querySelector('.ham-menu')
