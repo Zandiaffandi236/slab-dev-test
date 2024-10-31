e5a01c7271aa4625edbb2f55ea054adc37f8bc5e
Zandiaffandi236 <marzandi.leta@gmail.com>
2024-10-31 11:46:44 +0700

    add navbar section

---
+++ src/main.js
@@ -0,0 +1,19 @@
+import anime from 'animejs';
+import ScrollReveal from 'scrollreveal';
+
+// Contoh penggunaan ScrollReveal
+ScrollReveal().reveal('.nav-item', {
+  distance: '50px',
+  duration: 800,
+  easing: 'ease-out',
+  origin: 'top',
+  interval: 100  //
+})
+
+const hamMenu = document.querySelector('.ham-menu')
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+  hamMenu.classList.toggle('active');
+  offScreenMenu.classList.toggle('active');
+})
